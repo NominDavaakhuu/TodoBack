b50a12524f2ebdd68b628d8f1dc6e1b1f3fd1327
Nomin <nomin.davaakhuu@westpac.com.au>
2026-02-05 11:05:32 +1000

    added required data annotation to CreateDto

--- Shared/DTOs/TodoItem/TodoCreateDto.cs
+++ Shared/DTOs/TodoItem/TodoCreateDto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Shared.DTOs.TodoItem
 {
     public class TodoCreateDto
     {
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select a category")]
         public long? CategoryId { get; set; }
     }
 }
